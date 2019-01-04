b562d89729b7ee57a4fb596438bfc221f01be01e
David Herring <dave@netfm.org>
2019-01-04 17:18:40 +0000

    added tool-bar

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nw.settings.production")
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nw.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nw.settings.production")
 
     from django.core.management import execute_from_command_line
 
